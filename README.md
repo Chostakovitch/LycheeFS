34c08af6fcd018b88e62add94b70404214adee83
Quentin Duchemin <quentinduchemin@tuta.io>
2021-12-29 12:26:19 +0100

    Working skeleton for PEP517 build with setuptools

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lychee-fuse
+
 FUSE-based filesystem for Lychee, a photo management system.
+
+## Installation
+
+Requirements : 
+- FUSE and FUSE headers (`fuse` + `libfuse-dev` on Debian-like, `fuse3` on Arch)
+
+
---
+++ lychee_fuse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+# lychee-fuse: a FUSE-based filesystem for Lychee, a photo management system.
+
+For additonal information, visit :
+- [Lychee](https://github.com/LycheeOrg/Lychee)
+- [lychee-fuse](https://github.com/Chostakovitch/lychee-fuse)
+"""
